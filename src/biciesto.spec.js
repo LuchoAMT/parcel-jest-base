d62860a8befab5688135dd3c93aee864060324b6
Luis Menacho <luisantonio.menacho@gmail.com>
2023-08-23 11:23:01 -0400

    Year is not leap if can be divided by 100 and not by 400

--- src/biciesto.spec.js
+++ src/biciesto.spec.js
@@ -9,6 +9,10 @@ describe("Biciestos", () =>{
         expect(new Year(2001).isLeap()).toEqual(false);
     });
 
+    it("NO es bicieto si es divisible por 100 y no por 400", () =>{
+        expect(new Year(1700).isLeap()).toEqual(false);
+    });
+
     it("es bicieto si es divisible por 4 y no por 100", () =>{
         expect(new Year(2008).isLeap()).toEqual(true);
     });
