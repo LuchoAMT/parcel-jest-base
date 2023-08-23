77bb096163dfb55bdda499049fa4c5f195f55f42
Luis Menacho <luisantonio.menacho@gmail.com>
2023-08-23 11:30:11 -0400

    refactored: added methods for better self documentation

--- src/year.js
+++ src/year.js
@@ -3,8 +3,20 @@ class Year {
         this.year=year;
     }
     isLeap(){
-        if(this.year % 400 == 0) { return true; }
-        if(this.year % 4 == 0 && this.year % 100 != 0) { return true; }
+        if(this.isDivisible(400)) { return true; }
+        if(this.isDivisible(4) && this.isNotDivisible(100)) { return true; }
+        return false;
+    }
+    isDivisible(n){
+        if(this.year % n == 0){
+            return true;
+        }
+        return false;
+    }
+    isNotDivisible(n){
+        if(this.year % n != 0){
+            return true;
+        }
         return false;
     }
 }
